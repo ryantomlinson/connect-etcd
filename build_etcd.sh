42c3daf534829dd1147f8f895dbdd785b8dae28b
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-09-02 14:20:48 +0100

    missing tar extension for etcd download

--- build_etcd.sh
+++ build_etcd.sh
@@ -5,7 +5,7 @@ ${TRAVIS:?"This is not a Travis build. All Done"}
 sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
 
 wget https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz
-tar -zxvf etcd-v0.4.6-linux-amd64
+tar -zxvf etcd-v0.4.6-linux-amd64.tar.gz
 cd etcd-v0.4.6-linux-amd64
 ./etcd
 
