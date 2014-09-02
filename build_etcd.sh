42aaa53cb5208a353aa2d57d050e77f5e52372d8
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-09-02 14:26:30 +0100

    run etcd in background for travis

--- build_etcd.sh
+++ build_etcd.sh
@@ -7,6 +7,6 @@ sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
 wget https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz
 tar -zxvf etcd-v0.4.6-linux-amd64.tar.gz
 cd etcd-v0.4.6-linux-amd64
-./etcd
+./etcd &
 
 echo "All Done"
