8910a734181b17858f4a4b8522e4fed4462f8595
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-09-02 14:15:44 +0100

    changed build file to use binary

--- build_etcd.sh
+++ build_etcd.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-if [  $# -gt 0 ]
-    then
-    ETCD_VERSION="$1";
-    else
-    ETCD_VERSION="master";
-fi
-
-echo "Using ETCD version $ETCD_VERSION"
-
-git clone https://github.com/coreos/etcd.git
-cd etcd
-git checkout $ETCD_VERSION
-./build
-
-
 ${TRAVIS:?"This is not a Travis build. All Done"}
 #Temporal solution to travis issue #155
 sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
+
+wget https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz
+tar -zxvf etcd-v0.4.6-linux-amd64
+cd etcd-v0.4.6-linux-amd64
+./etcd
+
 echo "All Done"
